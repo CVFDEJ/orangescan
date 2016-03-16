494e10782e940ef41615b3bf8746a8debab61699
orange <orau9e@outlook.com>
2016-03-16 19:10:56 +0800

    重构

--- app/config.py
+++ app/config.py
@@ -7,8 +7,8 @@ info_db = redis.Redis(host='localhost', port=6379, db=2)
 log_db = redis.Redis(host='localhost', port=6379, db=3)
 # log_db -1 = 未扫描过子域名 0 = 扫描过子域名但未扫描详细信息 1 = 扫描过详细信息
 
-base_dir = '/Users/orange/Dropbox/OrangeScan/OrangeScan'
+base_dir = '/Users/orange/Dropbox/OrangeScan/OrangeScan/app'
 scan_dir = '%s/tool/' % base_dir
-scanpy = '%s/app/tool/scan.py' % base_dir
-infopy = '%s/app/tool/info.py' % base_dir
-log_dir = '%s/app/log' % base_dir
+scanpy = '%s/tool/scan.py' % base_dir
+infopy = '%s/tool/info.py' % base_dir
+log_dir = '%s/log' % base_dir
