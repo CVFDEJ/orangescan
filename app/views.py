d30f61ba5754d1d972ff23f5a2469d57cc7e9528
orange <orau9e@outlook.com>
2016-03-17 17:01:52 +0800

    fix bugs

--- app/views.py
+++ app/views.py
@@ -7,9 +7,16 @@ import re
 import psutil
 from flask import render_template
 from flask import request
+from flask import send_from_directory
 
 from app import app
 from app import config
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico')
 
 
 @app.route('/')
