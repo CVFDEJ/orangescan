0b573cb02260fcf122518f92203c71e20d199fca
orange <orau9e@outlook.com>
2016-03-17 16:34:23 +0800

    添加chrome插件

--- app/tool/config.py
+++ app/tool/config.py
@@ -7,8 +7,8 @@ info_db = redis.Redis(host='localhost', port=6379, db=2)
 log_db = redis.Redis(host='localhost', port=6379, db=3)
 # log_db -1 = 未扫描过子域名 0 = 扫描过子域名但未扫描详细信息 1 = 扫描过详细信息
 
-base_dir = '/Users/orange/Dropbox/OrangeScan/OrangeScan/app'
-tool_dir = '%s/tool/' % base_dir
-log_dir = '%s/tool/log' % base_dir
-scan_py = '%s/tool/scan.py' % base_dir
-info_py = '%s/tool/info.py' % base_dir
+base_dir = 'OrangeScan'
+tool_dir = '%s/app/tool/' % base_dir
+log_dir = '%s/app/tool/log' % base_dir
+scan_py = '%s/app/tool/scan.py' % base_dir
+info_py = '%s/app/tool/info.py' % base_dir
