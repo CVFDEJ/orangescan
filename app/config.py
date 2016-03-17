b626159f36a0c7e561ff182d8f0a1a8e6539caf5
orange <orau9e@outlook.com>
2016-03-17 17:12:25 +0800

    fix bugs

--- app/config.py
+++ app/config.py
@@ -1,4 +1,6 @@
 # coding=utf-8
+import os
+
 import redis
 
 queue_db = redis.Redis(host='localhost', port=6379, db=0)
@@ -6,7 +8,7 @@ domain_db = redis.Redis(host='localhost', port=6379, db=1)
 info_db = redis.Redis(host='localhost', port=6379, db=2)
 log_db = redis.Redis(host='localhost', port=6379, db=3)
 
-base_dir = 'OrangeScan'
+base_dir = os.getcwd()
 tool_dir = '%s/app/tool/' % base_dir
 log_dir = '%s/app/tool/log' % base_dir
 scan_py = '%s/app/tool/scan.py' % base_dir
