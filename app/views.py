033c6f054131c5d497696a6e11f696547a536031
orange <orau9e@outlook.com>
2016-03-18 10:43:13 +0800

    重构

--- app/views.py
+++ app/views.py
@@ -21,12 +21,12 @@ def favicon():
 
 
 @app.route('/')
-def index():
+def index_site():
     return render_template('index.html', title='首页')
 
 
 @app.route('/domain')
-def domain():
+def index_domain():
     return render_template('domain.html', title='子域名查询', action='/domain/search', domain='请输入企业一级域名，如：site.com, 支持模糊搜索')
 
 
@@ -40,7 +40,7 @@ def query_domain():
         return render_template('domain.html', subdomains=subdomains, title='查询结果 : %s' % domain,
                                action='/domain/search',
                                domain=domain, code=code)
-    elif regExp(q, qtype):
+    elif reg_exp(q, qtype):
         domain = q
         return render_template('domain.html', title='查询结果 : %s' % domain,
                                result='%s还未扫描, 当前cpu占用' % domain + str(psutil.cpu_percent()) + "%",
@@ -52,11 +52,11 @@ def query_domain():
 
 
 @app.route('/create/domain', methods=['POST', 'GET'])
-def new_task():
+def create_task():
     domain = request.args.get('q', '').lower()
     if config.domain_db.exists(domain):
         return render_template('task.html', result='任务已完成,点击查看', domain=domain)
-    elif regExp(domain, 'domain'):
+    elif reg_exp(domain, 'domain'):
         config.queue_db.set(domain, '0')
         config.log_db.set(domain, -1)
         os.system('python %s %s&' % (config.scan_py, domain))
@@ -66,9 +66,10 @@ def new_task():
 
 
 @app.route('/bak')
-def rdsbak():
+def bak_rds():
     os.system('cp %s/dump.rdb %s/bak/dump.rdb%d' % (config.redis_dir, config.redis_dir, int(time.time())))
     return 'ok'
+
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -76,18 +77,18 @@ def page_not_found(e):
 
 
 def search(q):
-    domaindb = config.domain_db
-    infodb = config.info_db
-    logdb = config.log_db
+    domain_db = config.domain_db
+    info_db = config.info_db
+    log_db = config.log_db
 
-    if logdb.keys('*%s*' % q):
-        domain = str(logdb.keys('*%s*' % q)[0], 'utf-8')
-        if infodb.exists(domain):
-            subdomains = choosedb(infodb, domain)
+    if log_db.keys('*%s*' % q):
+        domain = str(log_db.keys('*%s*' % q)[0], 'utf-8')
+        if info_db.exists(domain):
+            subdomains = choose_db(info_db, domain)
             os.system('python %s %s&' % (config.info_py, domain))
             code = '200'
         else:
-            subdomains = choosedb(domaindb, domain)
+            subdomains = choose_db(domain_db, domain)
             os.system('python %s %s&' % (config.info_py, domain))
             code = '302'
         return domain, subdomains, code
@@ -95,27 +96,27 @@ def search(q):
         return False
 
 
-def choosedb(db, q):
+def choose_db(db, q):
     subdomains = [str(i, 'utf-8') for i in db.smembers(q)]
     return subdomains
 
 
-def regExp(q, qtype):
+def reg_exp(q, qtype):
     if qtype == 'domain':
         return re.match(
-            '(^(?=^.{3,255}$)[a-zA-Z0-9][-a-zA-Z0-9]{0,62}(\.(com\.cn|tw|com|cn|net|cc|me|jp){0,65})+$)',
-            q) and re.match('^(?=^.{3,255}$)[a-zA-Z0-9][-a-zA-Z0-9]{0,62}(\.[a-zA-Z0-9][-a-zA-Z0-9]{0,62})+$', q)
+            '(^(?=^.{3,255}$)[a-z0-9][-a-z0-9]{0,62}(\.(com\.cn|tw|com|cn|net|cc|me|jp){0,65})+$)',
+            q) and re.match('^(?=^.{3,255}$)[a-z0-9][-a-zA-Z0-9]{0,62}(\.[a-z0-9][-a-z0-9]{0,62})+$', q)
     else:
         return False
 
 
-def getSubdomain():
+def get_subdomain():
     for task in config.queue_db.keys():
         if not config.log_db.exists(task):
             config.log_db.set(task, -1)
         elif config.log_db.get(task) == '-1':
             if int(os.popen('ps -h|grep scan.py|wc -l').read()) < 6:
-                os.system('python %s %s&' % (config.scanpy, str(task, 'utf-8')))
+                os.system('python %s %s&' % (config.scan_py, str(task, 'utf-8')))
             else:
                 pass
         elif config.log_db.get(task) == '0':
