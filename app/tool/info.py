b19d8c7eb3e0751f2ff55cda5be86b40b0a2bf5d
0xbug <orau9e@outlook.com>
2016-03-20 17:41:42 +0800

    Update info.py

--- app/tool/info.py
+++ app/tool/info.py
@@ -1,32 +1,17 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 import re
+import os
 import sys
 import threading
-import time
 from multiprocessing.dummy import Pool
-
 import lxml.html as H
 import requests
-
 import config
-
-reload(sys)
-sys.setdefaultencoding('utf8')
-lock = threading.Lock()
-threadpool = Pool(processes=100)
-TIMEOUT = 3
-scan_results = []
-
-
-def signal_handler(signal, frame):
-    threadpool.terminate()
-    threadpool.done = True
 
 
 def handle_result(host, port, result):
-    tm = time.time()
     with lock:
         scan_results.append([host, port, result])
 
@@ -37,11 +22,10 @@ def scan_scan(*kw):
     else:
         return
     result = None
-    rcode, server, title = 0, "", ""
+    httpcode, server, title = 0, "", ""
     target_url = "http://%s:%s" % (host, port)
     try:
         r = requests.get(target_url, timeout=TIMEOUT, allow_redirects=True)
-
         meta_reg = r"<meta.*?charset=['\"]?([^'\"><]*)?.*?>"
         if r.encoding == 'ISO-8859-1':
             m = re.search(meta_reg, r.text, re.I)
@@ -60,32 +44,31 @@ def scan_scan(*kw):
             title = "Null"
 
         if r.status_code:
-            rcode = r.status_code
+            httpcode = r.status_code
         if 'Server' in r.headers:
             server = r.headers['Server']
 
     except KeyboardInterrupt:
         pass
 
-    except requests.exceptions.Timeout, e:
+    except requests.exceptions.Timeout:
         pass
 
-    except Exception, e:
+    except:
         pass
 
     finally:
-        result = [target_url, rcode, server, title]
+        result = [target_url, httpcode, server, title]
 
     handle_result(host, port, result)
     return result
 
 
-def scan_file_check(domains, result_file=None):
+def scan_file_check(domain):
     port_list = [80]
-
     scan_list = []
-    for subdomain in domaindb.smembers(domains):
-        host = unicode(subdomain.strip(), 'utf-8')
+    for subdomains in domain_db.smembers(domain):
+        host = str(subdomains.strip(), 'utf-8')
         for port in port_list:
             scan_list.append([host, port])
 
@@ -99,37 +82,33 @@ def scan_file_check(domains, result_file=None):
         if x[2][1]:
             vul_results.append(x)
 
-    if result_file:
-        with open(result_file, 'w+') as f:
-            for x in vul_results:
-                h, p, r = x
-                rs = ""
-                tu, rcode, server, title = r
-                if title:
-                    title = title.replace("\r\n", "").replace("\n", "").replace("\r", "").replace("\t", "").replace(":",
-                                                                                                                    "")
-                    title = title.encode('utf-8')
-                if server:
-                    server = server.replace(":", "/")
-
-                rs = "%s\t%s\t%s\t%s" % (tu, rcode, server, title)
-                domain = tu.replace("http://", "").replace(":80", "")
-                try:
-                    if len(server) == 0: server = "Null"
-                    if len(title) == 0: title = "Null"
-                except:
-                    server = "Null"
-                    title = "Null"
-                result = "%s:%s:%s:%s" % (domain, server, rcode, title)
-                infodb.sadd(domains, result)
-                f.write("%s\n" % rs)
-
+    for x in vul_results:
+        h, p, r = x
+        tu, httpcode, server, title = r
+        if title:
+            title = title.replace("\r\n", "")\
+                    .replace("\n", "")\
+                    .replace("\r", "")\
+                    .replace("\t", "")\
+                    .replace(":", "")
+        if server:
+            server = server.replace(":", "/")
+        target = tu.replace("http://", "").replace(":80", "")
+        if len(server) == 0: server = "Null"
+        if len(title) == 0: title = "Null"
+        result = "%s:%s:%s:%s" % (target, server, httpcode, title)
+        info_db.sadd(domain, result)
+        log_db.set(domain, 1)
 
 if __name__ == '__main__':
-    domaindb = config.domain_db
-    infodb = config.info_db
-    logdb = config.log_db
-    logdir = config.log_dir
-    domains = sys.argv[1]
-    output_file = "%s/%s.txt" % (logdir, sys.argv[1])
-    scan_file_check(domains, output_file)
+    lock = threading.Lock()
+    threadpool = Pool(processes=100)
+    TIMEOUT = 3
+    scan_results = []
+
+    domain_db = config.domain_db
+    info_db = config.info_db
+    log_db = config.log_db
+
+    domain = sys.argv[1]
+    scan_file_check(domain)
